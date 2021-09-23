4a1712bc5f78bc6b99efa564aa4925e5b97dd9ff
Kyle Hanna <Hanna.Kyle@live.com>
2021-09-23 14:34:23 -0600

    fixing

--- src/services/NewsFeedService.js
+++ src/services/NewsFeedService.js
@@ -1,3 +1,7 @@
+import { AppState } from '../AppState'
+import { api } from './AxiosService'
+import { Post } from '../models/Post.js'
+
 class NewsFeedService {
   async getPosts() {
     const res = await api.get('api/blogs')
