ef63b7c8257980b2ad0d79ac812932ce66069652
Kyle Hanna <Hanna.Kyle@live.com>
2021-09-23 15:33:47 -0600

    read more

--- src/components/NewsFeed.vue
+++ src/components/NewsFeed.vue
@@ -1,13 +1,16 @@
 <template>
-  <div class="col-md-3">
+  <div class="col-md-3 py-3">
     <div class="card" style="">
-      <img :src="posts.imgUrl" class="card-img-top" alt="...">
+      <img :src="posts.imgUrl" class="card-img-top img-style" alt="...">
       <div class="card-body">
         <h5 class="card-title">
           {{ posts.title }}
         </h5>
-        <p class="card-text text-clip">
+        <p class="card-text clip-text" id="read-more">
           {{ posts.body }}
+        </p>
+        <p class="selectable " @click.prevent="readMore()">
+          Read more...
         </p>
         <a href="#" class="btn btn-primary">Go somewhere</a>
       </div>
@@ -28,13 +31,19 @@ export default {
   },
   setup(props) {
     return {
-      account: computed(() => AppState.account)
+      account: computed(() => AppState.account),
+      readMore() {
+        document.getElementById('read-more').classList.toggle('clip-text')
+      }
 
     }
   }
 }
 </script>
 
-<style>
-
+<style scoped lang="scss">
+.img-style{
+  height: 400px;
+  width: 450px;
+}
 </style>
